9f45d5c01e5b4239546701f4b55b36603c94e5aa
Raniyshka <raniya.good2@gmail.com>
2024-09-03 15:52:04 +0300

    практика 1

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// let name = prompt('Введите ваше имя', 'user');
+
+// alert(`Ваше имя ${name}`);
+
+// let isTrue = confirm('Это действительно ваше имя?');
+
+// alert(isTrue);
+
+alert('Я JavaScript');
+
+let name = prompt('Введите имя', 'user');
+let surname = prompt('Введите фамилию', 'user');
+let lastname = prompt('Введите отчество', 'user');
+
+alert(name + " " + surname + " " + lastname);
+
+let isTrue = confirm('Фио верны?');
+alert(isTrue);
